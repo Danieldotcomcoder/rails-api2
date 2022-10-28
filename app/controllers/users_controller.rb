e5527e0d7322b40ebdd302c7708f24ee22f131f7
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-10-28 12:11:03 +0300

    Reinitialized the project as API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,18 @@
-# frozen_string_literal: true
-
-module Api
-  class UsersController < ApplicationController
+class UsersController < ApplicationController
     before_action :authorize_request, except: :create
     before_action :find_user, except: %i[create index]
-
+  
     # GET /users
     def index
       @users = User.all
       render json: @users, status: :ok
     end
-
+  
     # GET /users/{username}
     def show
       render json: @user, status: :ok
     end
-
+  
     # POST /users
     def create
       @user = User.new(user_params)
@@ -26,7 +23,7 @@ module Api
                status: :unprocessable_entity
       end
     end
-
+  
     # PUT /users/{username}
     def update
       unless @user.update(user_params)
@@ -34,24 +31,23 @@ module Api
                status: :unprocessable_entity
       end
     end
-
+  
     # DELETE /users/{username}
     def destroy
       @user.destroy
     end
-
+  
     private
-
+  
     def find_user
       @user = User.find_by_username!(params[:_username])
-    rescue ActiveRecord::RecordNotFound
-      render json: { errors: 'User not found' }, status: :not_found
+      rescue ActiveRecord::RecordNotFound
+        render json: { errors: 'User not found' }, status: :not_found
     end
-
+  
     def user_params
       params.permit(
         :name, :username, :email, :password, :password_confirmation
       )
     end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   resources :users, param: :_username
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
-
 end
